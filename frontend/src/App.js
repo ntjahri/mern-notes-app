12b8038d8888c53213b29f8a3d7a5b90706282a3
ntjahri <jatinrr.2213@gmail.com>
2026-07-27 14:14:24 +0530

    Update API URL for production

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/notes';
+const API_URL = 'https://mern-notes-app-1-seeo.onrender.com';
 
 function App() {
   const [notes, setNotes] = useState([]);
